c5736eb78529c0d66efeea8435e1e9ba38f9dc3e
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-03-21 15:03:30 +0300

    Create Student.java

---
+++ OOP/10-interfaces/Student.java
@@ -0,0 +1,7 @@
+public class Student implements IDatabase{ //Student implements IDatabase(interface)
+
+    @Override
+    public void log() {
+        System.out.println("Student added...");
+    }
+}
